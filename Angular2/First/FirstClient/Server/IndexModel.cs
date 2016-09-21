bc47313a6806fd37c25e7bddc2dba6a15c9ea99f
Chris Hogan <hogancm@gmail.com>
2016-09-21 15:31:48 -0400

    In progress

--- Angular2/First/FirstClient/Server/IndexModel.cs
+++ Angular2/First/FirstClient/Server/IndexModel.cs
@@ -1,56 +1,78 @@
-﻿using Buddy.Nancy.Page;
+﻿using System.Reflection;
+using Buddy.Nancy.Page;
 using Buddy.Web;
 using Nancy;
 
 namespace FirstClient.Server
 {
-    public class IndexPageConfig : PageConfig
+    public class IndexModel : NancyModelBase
     {
-        public IndexPageConfig(NancyContext context)
-            :base(context)
-        {
-        }
-        }
-
-    public class IndexModel
-    {
-        public IndexModel(NancyContext context)
-        {
-            PageConfig = new IndexPageConfig(context);
-
 #if DEBUG
-            StyleSheets = new LinkBuilder
-            {
-                //new LinkDef {Href ="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css", Type = LinkType.Css, Rel = LinkRelType.Stylesheet }
-            };
-
-            
-            Scripts = new ScriptBuilder
-            {
-                new ScriptDef { Src = "node_modules/core-js/client/shim.js", Type = ScriptType.Javascript },
-                new ScriptDef { Src = "node_modules/zone.js/dist/zone.js", Type = ScriptType.Javascript },
-                new ScriptDef { Src = "node_modules/reflect-metadata/Reflect.js", Type = ScriptType.Javascript },
-                new ScriptDef { Src = "node_modules/systemjs/dist/system.src.js", Type = ScriptType.Javascript },
-                new ScriptDef { Src = "Client/system.config.js", Type = ScriptType.Javascript },
-                new ScriptDef { Body = "System.import('app').catch (function(err){ console.error(err); })", Type = ScriptType.Javascript }
-            };
+        protected readonly bool Debug = true;
 #else
-            StyleSheets = new StyleSheets
-            {
-                new LinkDef {Href ="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css", Rel="stylesheet", Type="text/css" }
-            };
-
-            Scripts = new Scripts
-            {
-                new ScriptDef { Src = "https://cdnjs.cloudflare.com/ajax/libs/angular.js/2.0.0-beta.17/angular2.min.js", Type = "text/javascript" }
-            };
+        protected readonly bool Debug = false;
 #endif
+
+
+        public IndexModel(NancyContext context)
+            : base(context)
+        {
         }
 
-        public IndexPageConfig PageConfig { get; private set; }
-        public ScriptBuilder Scripts { get; private set; }
-        public LinkBuilder StyleSheets { get; private set; }
+        protected override NancyPageConfig InitPageConfig()
+        {
+            return new IndexPageConfig(Context, Assembly.GetExecutingAssembly(), Debug);
+        }
+
+        protected override LinkBuilder InitFavIcons()
+        {
+            return base.InitFavIcons();
+        }
+        
+        protected override LinkBuilder InitStylesheets()
+        {
+            if (Debug)
+            {
+                return new LinkBuilder
+                {
+                    new LinkDef {Href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css", Type = LinkType.Css, Rel = LinkRelType.Stylesheet},
+                    new LinkDef {Href = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css", Type = LinkType.Css, Rel = LinkRelType.Stylesheet}
+                };
+            }
+            else
+            {
+                return new LinkBuilder
+                {
+                    new LinkDef {Href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css", Type = LinkType.Css, Rel = LinkRelType.Stylesheet},
+                    new LinkDef {Href = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css", Type = LinkType.Css, Rel = LinkRelType.Stylesheet}
+                };
+            }
+        }
 
 
+        protected override ScriptBuilder InitScripts()
+        {
+            if (Debug)
+            {
+                return new ScriptBuilder
+                {
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/shim.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/zone.js/0.6.25/zone.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/reflect-metadata/0.1.8/Reflect.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.38/system.src.js", Type = ScriptType.Javascript}
+
+                };
+            }
+            else
+            {
+                return new ScriptBuilder
+                {
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/shim.min.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/zone.js/0.6.25/zone.min.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/reflect-metadata/0.1.8/Reflect.min.js", Type = ScriptType.Javascript},
+                    new ScriptDef {Src = "https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.19.38/system.src.js", Type = ScriptType.Javascript}
+                };
+            }
+        }
     }
 }
---
+++ Angular2/First/FirstClient/Server/IndexPageConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Buddy.Nancy.Page;
+using Nancy;
+
+namespace FirstClient.Server
+{
+    public class IndexPageConfig : NancyPageConfig
+    {
+        public IndexPageConfig(NancyContext context, Assembly versionAssembly, bool debug)
+            :base(context, versionAssembly, debug)
+        {
+        }
+    }
+}
