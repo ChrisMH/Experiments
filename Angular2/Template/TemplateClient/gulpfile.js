bcbedecfe1c4055872d9def7fa3e144059ff3788
Chris Hogan <hogancm@gmail.com>
2016-10-03 11:05:18 -0400

    Updated Angular2 Template packages

--- Angular2/Template/TemplateClient/gulpfile.js
+++ Angular2/Template/TemplateClient/gulpfile.js
@@ -5,221 +5,214 @@ var gConcat = require("gulp-concat");
 var gDebug = require("gulp-debug");
 var gDotNetAssemblyInfo = require("gulp-dotnet-assembly-info");
 var gIf = require("gulp-if");
-var gInlineNg2Template = require("gulp-inline-ng2-template");
+var gInsert = require("gulp-insert");
 var gLess = require("gulp-less");
 var gRename = require("gulp-rename");
 var gSourceMaps = require("gulp-sourcemaps");
 var gStreamify = require("gulp-streamify");
+var gTypescript = require("gulp-typescript");
 var gUglify = require("gulp-uglify");
 var gUtil = require("gulp-util");
 
-var browserify = require("browserify");
 var del = require("del");
 var fs = require("fs");
 var mergeStream = require("merge-stream");
+var runSequence = require("run-sequence");
+var systemjsBuilder = require("systemjs-builder");
 var vSourceStream = require("vinyl-source-stream");
-var tsify = require("tsify");
 
-var vendorStylesheetFiles = [
-    "styles/vendor/font-awesome/less/font-awesome.less",
-    "styles/vendor/bootstrap/less/bootstrap.less"
+
+var vendorJavascript = [
+    { src: "node_modules/core-js/client/shim.js", dst: "js/vendor/core-js", version: function () { return getPackageVersion("core-js") } },
+    { src: "node_modules/zone.js/dist/zone.js", dst: "js/vendor/zone.js" version: function () { return getPackageVersion("zone.js") } },
+    { src: "node_modules/reflect-metadata/Reflect.js", dst: "js/vendor/reflect-metadata", version: function () { return getPackageVersion("reflect-metadata") } },
+    { src: "node_modules/systemjs/dist/system.src.js", dst: "js/vendor/systemjs", rename: "system.js", version: function () { return getPackageVersion("systemjs") } },
+
+    //{ src: "node_modules/typescript/lib/typescript.js", dst: "js/vendor/typescript", version: function () { return getPackageVersion("typescript") } },
+    //{ src: "node_modules/systemjs-plugin-text/text.js", dst: "js/vendor/systemjs", rename: "plugin-text.js", version: function () { return getPackageVersion("systemjs-plugin-text") } },
+    //{ src: "node_modules/systemjs-plugin-babel/plugin-babel.js", dst: "js/vendor/systemjs", version: function () { return getPackageVersion("systemjs-plugin-babel") } },
+    //{ src: "node_modules/systemjs-plugin-babel/systemjs-babel-browser.js", dst: "js/vendor/systemjs", version: function () { return getPackageVersion("systemjs-plugin-babel") } },
 ];
 
-var vendorJavascriptFiles = [
-    { src: "node_modules/core-js/client/shim.js", dst: "js/vendor/core-js" },
-    { src: "node_modules/zone.js/dist/zone.js", dst: "js/vendor/zone.js" },
-    { src: "node_modules/reflect-metadata/Reflect.js", dst: "js/vendor/reflect-metadata" },
-    { src: "node_modules/systemjs/dist/system.src.js", dst: "js/vendor/systemjs", rename: "system.js" },
-    { src: "node_modules/@angular/common/bundles/common.umd.js", dst: "js/vendor/@angular/" },
-    { src: "node_modules/@angular/compiler/bundles/compiler.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/core/bundles/core.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/forms/bundles/forms.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/http/bundles/http.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/platform-browser/bundles/platform-browser.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js", dst: "js/vendor/@angular" },
-    { src: "node_modules/@angular/router/bundles/router.umd.js", dst: "js/vendor/@angular" },
-    //{ src: "", dst: "" },
+var vendorStylesheets = [
+    { src: "node_modules/bootstrap/dist/css/bootstrap.css", dst: "css/vendor/bootstrap", version: function () { return getPackageVersion("bootstrap") } },
 ];
 
-
-var appStylesheetFiles = [
-    "styles/app/app.less"
+var vendorArtifacts = [
+    { src: "node_modules/bootstrap/dist/fonts/**/*", dst: "css/vendor/bootstrap/fonts" }
 ];
 
-var styleFiles = [
-    { src: "styles/app/images/**/*", dst: "css/images" },
-    { src: "styles/vendor/font-awesome/fonts/**/*", dst: "css/fonts" },
-    { src: "styles/vendor/bootstrap/fonts/**/*", dst: "css/fonts" }
+var appStylesheets = [
+    { src: "styles/app/app.less", dst: "css/app", version: getAppVersion }
 ];
 
-var bootJavascriptFiles = [
-    "node_modules/core-js/client/shim.js",
-    "node_modules/zone.js/dist/zone.js",
-    "node_modules/reflect-metadata/Reflect.js",
-    "node_modules/systemjs/dist/system.src.js"
+var appHtml = [
+    { src: "scripts/app/**/*.html", dst: "scripts/app", moduleName: "app-templates", rename: "app-templates.js" }
+]
+
+var appArtifacts = [
+    { src: "styles/app/images/**/*", dst: "css/app/images" }
 ];
 
-var libraryModules = [
-    "@angular/common",
-    "@angular/compiler",
-    "@angular/core",
-    "@angular/forms",
-    "@angular/http",
-    "@angular/platform-browser",
-    "@angular/platform-browser-dynamic",
-    "@angular/router",
-
-    "rxjs/Rx",
-    "typedjson"
-];
-
-var getPackageJson = function() {
-    return JSON.parse(fs.readFileSync("./package.json", "utf8"));
-};
-
-var getAssemblyInfo = function() {
-    var fileContents = fs.readFileSync("./Properties/AssemblyInfo.cs");
-    return gDotNetAssemblyInfo.getAssemblyMetadata(fileContents);
-};
 
 gulp.task("watch",
     function ()
     {
         gulp.watch(["styles/app/**/*.less"], ["build:dev:app:css"]);
         gulp.watch(["styles/vendor/**/*.less", "styles/vendor/**/*.css"], ["build:dev:vendor:css"]);
+        gulp.watch(["scripts/app/**/*.html"], ["build:dev:app:html"]);
     });
 
 
-gulp.task("build:dev", ["build:dev:app:css", "build:dev:vendor:css", "build:dev:vendor:js", "copy:styles"], function () { });
-gulp.task("build:prod", ["build:prod:app:css", "build:prod:vendor:css", "build:prod:vendor:js", "build:prod:app:js", "copy:styles"], function () { });
-
-
-gulp.task("clean", ["clean:css", "clean:js", "clean:styles"], function () {});
-
-gulp.task("clean:css", function ()
+gulp.task("build:dev", function (cb)
 {
-    return del("css");
+    runSequence(["build:dev:vendor:css", "build:dev:vendor:js", "copy:vendor:artifacts",
+                 "build:dev:app:css", "build:dev:app:html", "copy:app:artifacts"], cb);
 });
 
-gulp.task("clean:js", function ()
+gulp.task("build:prod",function (cb)
 {
-    return del(["js", "scripts/app/**/*.js", "scripts/app/**/*.map", "!scripts/app/system.dev.js"]);
+    runSequence("clean",
+                ["build:prod:vendor:css", "build:prod:vendor:js", "copy:vendor:artifacts", "build:prod:app:css", "copy:app:artifacts"],
+                "clean:scripts", cb);
 });
 
-gulp.task("clean:styles", function ()
-{   
-    var promise = [];
-    styleFiles.forEach(function (styleFile)
-    {
-        promise.push(del(styleFile.dst));
-    });
-    return Promise.all(promise);
-});
-
-gulp.task("build:inline:templates", function ()
+gulp.task("clean", ["clean:js", "clean:scripts", "clean:css"], function () {});
+gulp.task("clean:js", function () { return del(["js"]); });
+gulp.task("clean:scripts", function ()
 {
-    return gulp.src("scripts/app/**/*.ts")
-               .pipe(gInlineNg2Template({ base: "/", useRelativePaths: true }))
-               .pipe(gulp.dest("js/app"));
+    return del(["scripts/app/**/*.js", "scripts/app/**/*.map",
+                "!scripts/app/system.config.dev.js", "!scripts/app/system.config.prod.js"]);
 });
+gulp.task("clean:css", function () { return del(["css"]); });
 
-gulp.task("build:prod:app:js", ["build:inline:templates"], function ()
+
+// Vendor Javascript
+
+gulp.task("build:dev:vendor:js", function () { return buildJavascript(vendorJavascript, false); });
+gulp.task("build:prod:vendor:js", function ()  { return buildJavascript(vendorJavascript, true); });
+
+
+// Vendor Stylesheets
+
+gulp.task("build:dev:vendor:css", function () { return buildStylesheets(vendorStylesheets, false); });
+gulp.task("build:prod:vendor:css", function () { return buildStylesheets(vendorStylesheets, true); });
+
+// Vendor Artifacts
+
+gulp.task("copy:vendor:artifacts", function () { return copyArtifacts(vendorArtifacts); });
+
+
+// App Javascript
+
+
+// App Stylesheets
+
+gulp.task("build:dev:app:css", function () { return buildStylesheets(appStylesheets, false); });
+gulp.task("build:prod:app:css", function () { return buildStylesheets(appStylesheets, true); });
+
+// App HTML
+
+gulp.task("build:dev:app:html", function() { return buildHtml(appHtml, false); });
+gulp.task("build:prod:app:html", function() { return buildHtml(appHtml, true); });
+
+// App Artifacts
+
+gulp.task("copy:app:artifacts", function () { return copyArtifacts(appArtifacts); });
+
+// Helper functions
+
+function getAppVersion()
 {
-    var b = browserify({
-        debug: false
-    });
+    var assemblyInfoFile = fs.readFileSync("./Properties/AssemblyInfo.cs");
+    return gDotNetAssemblyInfo.getAssemblyMetadata(assemblyInfoFile).AssemblyVersion;
+}
 
-    b.add("js/app/main.prod.ts");
-    libraryModules.forEach(function (lib) { b.external(lib); });
+function getPackageVersion(packageKey)
+{
+    var packageFile = JSON.parse(fs.readFileSync("./package.json", "utf8"));
+    var version = "0.0.0";
 
-    return b.plugin(tsify).bundle().on("error", function (err) { gUtil.log(err); })
-            .pipe(vSourceStream("app-" + getAssemblyInfo().AssemblyVersion + ".min.js"))
-            .pipe(gStreamify(gUglify()))
-            .pipe(gulp.dest("js"));
-});
+    if (packageFile.dependencies && packageFile.dependencies.hasOwnProperty(packageKey))
+        version = packageFile.dependencies[packageKey];
+    else if(packageFile.devDependencies && packageFile.devDependencies.hasOwnProperty(packageKey))
+        version = packageFile.devDependencies[packageKey];
+        
+    if(version[0] === "^" || version[0] === "~")
+        return version.substr(1, version.length - 1);
+    return version;
+}
 
+function getTypescriptConfig()
+{
+    var tsConfigFile = JSON.parse(fs.readFileSync("./tsconfig.json", "utf8"));
+    return tsConfigFile.compilerOptions;
+}
 
-gulp.task("build:dev:vendor:js", function ()
+function buildStylesheets(files, compress)
 {
     var streams = [];
-    vendorJavascriptFiles.forEach(function (file)
+    files.forEach(function (file)
     {
         streams.push(
             gulp.src(file.src)
-                .pipe(gIf((file.rename != undefined), gRename(file.rename)))
+                .pipe(gIf(file.rename != undefined, gRename(file.rename)))
+                .pipe(gIf(file.version != undefined, gRename(function (path) { path.basename += "-" + file.version(); })))
+                .pipe(gIf(/[.]less/, gLess(), gRename(function (path) { path.extname = ".css"; })))
+                .pipe(gIf(compress, gCleanCss({ keepSpecialComments: 0 })))
                 .pipe(gulp.dest(file.dst))
         );
     });
     return mergeStream(streams);
-});
+}
 
-gulp.task("build:prod:vendor:js", function ()
+function buildJavascript(files, compress)
 {
     var streams = [];
-    vendorJavascriptFiles.forEach(function (file)
+    files.forEach(function (file)
     {
         streams.push(
             gulp.src(file.src)
-                .pipe(gIf((file.rename != undefined), gRename(file.rename)))
-                .pipe(gStreamify(gUglify()))
+                .pipe(gIf(file.rename != undefined, gRename(file.rename)))
+                .pipe(gIf(file.version != undefined, gRename(function (path) { path.basename += "-" + file.version(); })))
+                //.pipe(gIf(/[.]ts/, gTypescript(getTypescriptConfig())))
+                .pipe(gIf(compress, gStreamify(gUglify())))
                 .pipe(gulp.dest(file.dst))
         );
     });
     return mergeStream(streams);
-});
+}
 
 
-
-gulp.task("build:dev:app:css", function ()
-{
-    return gulp.src(appStylesheetFiles)
-               .pipe(gSourceMaps.init())
-               //.pipe(gDebug({ title: "style files: " }))
-               .pipe(gIf(/[.]less/, gLess()))
-               .pipe(gSourceMaps.write())
-               .pipe(gConcat("app-" + getAssemblyInfo().AssemblyVersion + ".css"))
-               .pipe(gulp.dest("css"));
-});
-
-gulp.task("build:prod:app:css", function ()
-{
-    return gulp.src(appStylesheetFiles)
-                //.pipe(gDebug({ title: "style files: " }))
-                .pipe(gIf(/[.]less/, gLess()))
-                .pipe(gConcat("app-" + getAssemblyInfo().AssemblyVersion + ".min.css"))
-                .pipe(gCleanCss({ keepSpecialComments: 0}))
-                .pipe(gulp.dest("css"))
-});
-
-gulp.task("build:dev:vendor:css", function ()
-{
-    return gulp.src(vendorStylesheetFiles)
-               .pipe(gSourceMaps.init())
-               //.pipe(gDebug({ title: "style files: " }))
-               .pipe(gIf(/[.]less/, gLess()))
-               .pipe(gSourceMaps.write())
-               .pipe(gConcat("vendor-" + getAssemblyInfo().AssemblyVersion + ".css"))
-               .pipe(gulp.dest("css"));
-});
-
-gulp.task("build:prod:vendor:css", function ()
-{
-    return gulp.src(vendorStylesheetFiles)
-                //.pipe(gDebug({ title: "style files: " }))
-                .pipe(gIf(/[.]less/, gLess()))
-                .pipe(gConcat("vendor-" + getAssemblyInfo().AssemblyVersion + ".min.css"))
-                .pipe(gCleanCss({ keepSpecialComments: 0 }))
-                .pipe(gulp.dest("css"))
-});
-
-
-gulp.task("copy:styles", function ()
+function buildHtml(files, compress)
 {
     var streams = [];
-    styleFiles.forEach(function (style)
+    files.forEach(function (file)
     {
-        streams.push(gulp.src(style.src).pipe(gulp.dest(style.dst)));
+        streams.push(
+            gulp.src(file.src)
+                .pipe(ghtml2js("templates.js", {
+                    adapter: "angular",
+                    base: "",
+                    name: file.moduleName
+                }))
+               .pipe(gInsert.prepend("require('angular');"))
+               .pipe(gRename(file.rename))
+               .pipe(gIf(file.version != undefined, gRename(function (path) { path.basename += "-" + file.version(); })))
+               .pipe(gulp.dest(file.dst))
+            );
     });
     return mergeStream(streams);
-});
+}
+
+
+function copyArtifacts(artifacts)
+{
+    var streams = [];
+    artifacts.forEach(function (artifact)
+    {
+        streams.push(gulp.src(artifact.src).pipe(gulp.dest(artifact.dst)));
+    });
+    return mergeStream(streams);
+}
 
