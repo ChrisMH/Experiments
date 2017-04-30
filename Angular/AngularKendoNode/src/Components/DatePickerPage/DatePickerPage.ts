09ff57f8112e003486bfe791bbbfe9c72e4f2aaf
Chris Hogan <hogancm@gmail.com>
2017-04-30 11:45:07 -0400

    Moved components around in Angular experiments

---
+++ Angular/AngularKendoNode/src/Components/DatePickerPage/DatePickerPage.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "date-picker-page",
+    templateUrl: "./DatePickerPage.html",
+    styleUrls: ["./DatePickerPage.css"],
+    host: { class: "view" }
+})
+
+export class DatePickerPage
+{
+}
+   
---
+++ Angular/AngularNet/AngularNet/src/Components/App/App.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+import { AppSettings } from "../../Services";
+
+@Component({
+    moduleId: module.id,
+    selector: "app",
+    templateUrl: "./App.html",
+    styleUrls: ["./App.css"]
+})
+export class App {
+    constructor(private appSettings: AppSettings) { }
+} 
+ 
