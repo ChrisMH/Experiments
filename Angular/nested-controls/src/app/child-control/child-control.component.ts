2d3475f01528665a968c97e015ddd81d9b48f6c6
Chris Hogan <hogancm@gmail.com>
2018-03-02 10:45:18 -0500

    Parent/Child array stuff working

--- Angular/nested-controls/src/app/child-control/child-control.component.ts
+++ Angular/nested-controls/src/app/child-control/child-control.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, IterableChangeRecord, IterableDiffer,
+    OnChanges, OnInit, DoCheck, Output, SimpleChanges, IterableDiffers } from '@angular/core';
+
+import { Company } from '../models';
 
 @Component({
-  selector: 'app-child-control',
-  templateUrl: './child-control.component.html',
-  styleUrls: ['./child-control.component.styl']
+    selector: 'app-child-control',
+    templateUrl: './child-control.component.html',
+    styleUrls: ['./child-control.component.styl']
 })
-export class ChildControlComponent implements OnInit {
+export class ChildControlComponent implements OnInit, OnChanges, DoCheck {
+    @Input() companies: Company[] = [];
 
-  constructor() { }
+    //private _companies: Company[] = [];
 
-  ngOnInit() {
-  }
+    // @Input() set companies(companies: Company[]) {
+    //     this._companies = companies;
+    //     // if (this._companies.length > 0) {
+    //     //     setTimeout(() => this.sortCompanies(), 1000);
+    //     // }
+    // }
+    // @Output() companiesChange = new EventEmitter<Company[]>();
+    private companiesDiffer: IterableDiffer<Company>;
 
+    constructor(protected iterDiffer: IterableDiffers) {
+        this.companiesDiffer = this.iterDiffer.find(this.companies).create(null);
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if ('companies' in changes) {
+            console.log('Child : ngOnChanges : companies changed');
+            this.companiesDiffer = this.iterDiffer.find(this.companies).create(null);
+            setTimeout(() => this.sortCompanies());
+        }
+    }
+
+    ngDoCheck(): void {
+        const changes = this.companiesDiffer.diff(this.companies);
+
+        if (changes) {
+            console.log('Child : ngDoCheck : changes');
+            changes.forEachAddedItem((i: IterableChangeRecord<Company>) =>
+                console.log(`  added: id=${i.item.id}, displayName=${i.item.displayName}`));
+            changes.forEachIdentityChange((i: IterableChangeRecord<Company>) =>
+                console.log(`  identity: id=${i.item.id}, displayName=${i.item.displayName}`));
+            changes.forEachMovedItem((i: IterableChangeRecord<Company>) =>
+                console.log(`  moved: id=${i.item.id}, displayName=${i.item.displayName}`));
+            changes.forEachRemovedItem((i: IterableChangeRecord<Company>) =>
+                console.log(`  removed: id=${i.item.id}, displayName=${i.item.displayName}`));
+            setTimeout(() => this.sortCompanies());
+        }
+        else {
+            console.log('Child : ngDoCheck : no changes');
+        }
+    }
+
+    protected sortCompanies() {
+        console.log('sortCompanies');
+        this.companies.sort((a: Company, b: Company) => {
+            if (a.id < b.id) {
+                return -1;
+            }
+            else if (a.id > b.id) {
+                return 1;
+            }
+            return 0;
+        });
+        // this.companiesChange.emit(this._companies);
+        // setTimeout(() => this.addCompany(), 1000);
+    }
+
+    private id = 2;
+    onAdd() {
+        this.companies.push({id: this.id, displayName: `Company ${this.id}`});
+        this.id++;
+    }
+
+    onClear() {
+        this.companies.splice(0);
+    }
 }
--- Angular/nested-controls/src/app/parent-control/parent-control.component.ts
+++ Angular/nested-controls/src/app/parent-control/parent-control.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+import { Company } from '../models';
 
 @Component({
-  selector: 'app-parent-control',
-  templateUrl: './parent-control.component.html',
-  styleUrls: ['./parent-control.component.styl']
+    selector: 'app-parent-control',
+    templateUrl: './parent-control.component.html',
+    styleUrls: ['./parent-control.component.styl']
 })
-export class ParentControlComponent implements OnInit {
+export class ParentControlComponent implements OnInit, OnChanges {
 
-  constructor() { }
+    companies: Company[] = [];
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+        setTimeout(() => this.createCompanies(), 1000);
+
+        // setTimeout(() => console.dir(this.companies), 6000);
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.dir(changes);
+    }
+
+    protected createCompanies() {
+        // const newCompanies: Company[] = [];
+        this.companies.push({id: 100, displayName: 'Company 100'});
+        this.companies.push({id: 50, displayName: 'Company 50'});
+        this.companies.push({id: 106, displayName: 'Company 106'});
+        this.companies.push({id: 1, displayName: 'Company 1'});
+        // this.companies = newCompanies;
+    }
+
+    private id = 51;
+    onAdd() {
+        this.companies.push({id: this.id, displayName: `Company ${this.id}`});
+        this.id++;
+    }
 }
