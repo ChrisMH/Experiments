287179973962ac7b2c22718ddbdfe51a5f298018
Chris Hogan <hogancm@gmail.com>
2018-04-11 11:40:18 -0400

    Layout in angular-ngrx

--- Angular/angular-ngxs/src/app/app.module.ts
+++ Angular/angular-ngxs/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
 
+const routes: Routes =
+    [
+        { path: '', component: FirstPageComponent },
+        { path: 'second', component: SecondPageComponent },
+        { path: '**', redirectTo: '/', pathMatch: 'full' }
+    ];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FirstPageComponent,
+        SecondPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
