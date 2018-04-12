d7b469f9066a51167d91cd8c8706ed5026c0d6fb
Chris Hogan <hogancm@gmail.com>
2018-04-12 11:03:09 -0400

    Added Action to RouterState

--- Angular/angular-ngxs/src/app/store/router.state.ts
+++ Angular/angular-ngxs/src/app/store/router.state.ts
@@ -2,6 +2,11 @@ import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 import { Action, State, StateContext } from '@ngxs/store';
 import { filter } from 'rxjs/operators';
 
+class Navigated {
+    static readonly type = '[router] Navigated';
+    constructor(public url: string) {}
+}
+
 @State<string>({
     name: 'router',
     defaults: ''
@@ -16,7 +21,13 @@ export class RouterState {
         this.router.events.pipe(
             filter((event: RouterEvent) => event instanceof NavigationEnd)
         ).subscribe((event: RouterEvent) => {
-            state.setState(event.url);
+            state.dispatch(new Navigated(event.url));
+            // state.setState(event.url);
         });
+    }
+
+    @Action(Navigated)
+    navigated({setState}: StateContext<string>, {url}: Navigated) {
+        setState(url);
     }
 }
